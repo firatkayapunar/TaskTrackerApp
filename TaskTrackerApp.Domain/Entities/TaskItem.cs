fd4ff13adc0d3a752a4a8e1e5f096018902cce8f
Fırat Kayapunar <kayapunar.firat@gmail.com>
2025-07-29 17:40:33 +0300

    feat(domain): add core entities, auditing interfaces, and task state enum
    Added TaskItem and User entities representing the core domain models.
    Introduced IAuditable and IFullAuditable interfaces along with base auditing classes for consistent tracking of entity lifecycle events.
    
    Also defined TaskState enum to represent the status of a task (e.g., Pending, InProgress, Completed), ensuring domain-level state encapsulation.
    
    Folder structure organized into Entities, Enums, Common, and Interfaces to support Screaming Architecture principles.

---
+++ TaskTrackerApp.Domain/Entities/TaskItem.cs
@@ -0,0 +1,27 @@
+﻿using TaskTrackerApp.Domain.Common;
+using TaskTrackerApp.Domain.Enums;
+using TaskTrackerApp.Domain.Interfaces;
+
+namespace TaskTrackerApp.Domain.Entities;
+
+public class TaskItem : IEntity, IFullAuditable
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = default!;
+    public string? Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool IsCompleted { get; set; }
+
+    public TaskState State{ get; set; } = TaskState.NotStarted;
+
+    public Guid UserId { get; set; }
+    public User User { get; set; } = default!;
+
+    public DateTime CreatedAt { get; set; }
+    public string CreatedBy { get; set; } = "System";
+    public DateTime? UpdatedAt { get; set; }
+    public string? UpdatedBy { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public string? DeletedBy { get; set; }
+    public bool IsDeleted { get; set; }
+}
