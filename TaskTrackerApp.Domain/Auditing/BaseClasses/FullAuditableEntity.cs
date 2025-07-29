dec8527bb5c5892513cfd08a107004b26aa28e36
Fırat Kayapunar <kayapunar.firat@gmail.com>
2025-07-29 19:02:22 +0300

    refactor(domain): restructure auditing components and improve folder organization
    Refactored auditing logic by introducing separate folders for BaseClasses and Interfaces under the Auditing module.
    This improves modularity and supports Screaming Architecture by clearly isolating responsibilities.
    
    Also cleaned up Domain structure to enhance clarity and enforce separation of concerns, including clearer segregation for Entity, Enum, and Common layers.

---
+++ TaskTrackerApp.Domain/Auditing/BaseClasses/FullAuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿using TaskTrackerApp.Domain.Auditing.Interfaces;
+
+namespace TaskTrackerApp.Domain.Auditing.BaseClasses;
+
+public abstract class FullAuditableEntity : AuditableEntity, IFullAuditable
+{
+    public bool IsDeleted { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public string? DeletedBy { get; set; }
+}
---
+++ TaskTrackerApp.Domain/Common/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskTrackerApp.Domain.Common;
+
+public abstract class BaseEntity : IEntity
+{
+    public Guid Id { get; set; }
+}
